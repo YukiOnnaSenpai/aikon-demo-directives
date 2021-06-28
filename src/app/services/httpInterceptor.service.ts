5aa24159c15af353fd574ef32eeaef47a88f6c74
Sofija <smatanovic@aikongroup.com>
2021-06-28 20:45:43 +0200

    changed interceptor

--- src/app/services/httpInterceptor.service.ts
+++ src/app/services/httpInterceptor.service.ts
@@ -1,18 +1,41 @@
-import { HttpHeaders, HttpClient, HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { HttpHeaders, HttpClient, HttpInterceptor,
+  HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-
+import { Observable, throwError } from 'rxjs';
+import { HomeService } from './home.service';
+import { catchError, finalize } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>,
-    next: HttpHandler): Observable<HttpEvent<any>> {
+  constructor(
+    private homeService: HomeService,
+  ) {}
+/*
+  intercept(req: HttpRequest<unknown>,
+    next: HttpHandler): Observable<HttpEvent<unknown>> {
+      this.homeService.getAll();
+      return next.handle(req).pipe(
+        catchError((error: HttpErrorResponse) => {
+          console.log("PRODAVNICA HOSTNAME IZVOLITE >>>>>> " + location.hostname);
+          this.homeService.getAll();
 
+          return throwError(error);
+        }),
+        finalize(() => {
+          console.log('Nocni klub i svi smo tu.');
 
-    console.log("PRODAVNICA HOSTNAME IZVOLITE >>>>>> " + location.hostname);
+        })
+      ) as Observable<HttpEvent<any>>;
 
+    
+
+  } */
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log("nemza govno" +location.hostname);
+
+    return next.handle(request);
   }
 
 
